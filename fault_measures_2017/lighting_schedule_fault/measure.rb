289db1592322b5ddf406f42c15c88a4a1eee5cd4
geekshock <kimjanghyun84@gmail.com>
2017-07-11 17:06:33 -0400

    Update measure.rb

--- fault_measures_2017/lighting_schedule_fault/measure.rb
+++ fault_measures_2017/lighting_schedule_fault/measure.rb
@@ -14,12 +14,12 @@ class LightingScheduleFault < OpenStudio::Ruleset::ModelUserScript
 
   # human readable description
   def description
-    return "Replace this text with an explanation of what the measure does in terms that can be understood by a general building professional audience (building owners, architects, engineers, contractors, etc.).  This description will be used to create reports aimed at convincing the owner and/or design team to implement the measure in the actual building design.  For this reason, the description may include details about how the measure would be implemented, along with explanations of qualitative benefits associated with the measure.  It is good practice to include citations in the measure if the description is taken from a known source or if specific benefits are listed."
+    return "This fault characterizes additional lighting energy use due to either improper lighting schedules or failure of occupants to turn off lights when vacating a building."
   end
 
   # human readable description of modeling approach
   def modeler_description
-    return "Replace this text with an explanation for the energy modeler specifically.  It should explain how the measure is modeled, including any requirements about how the baseline model must be set up, major assumptions, citations of references to applicable modeling resources, etc.  The energy modeler should be able to read this description and understand what changes the measure is making to the model and why these changes are being made.  Because the Modeler Description is written for an expert audience, using common abbreviations for brevity is good practice."
+    return "Excessive lighting energy use is simulated by defining the elapsed time of delayed onset and early termination in hours. Additional option for not having setback control is also included. The fault intensity is defined as the percentage of increased lighting operation time compared to the non-faulted operation."
   end
 
   # define the arguments that the user will input
