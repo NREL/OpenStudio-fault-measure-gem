90d429a589579fbe188d11e82e89c6fac7ad2426
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2017-11-08 15:46:25 -0500

    Update measure.rb

--- fault_measures_2017/ThermostatBias/measure.rb
+++ fault_measures_2017/ThermostatBias/measure.rb
@@ -22,7 +22,7 @@ class ThermostatBias < OpenStudio::Ruleset::ModelUserScript
   #define the name that a user will see, this method may be deprecated as
   #the display name in PAT comes from the name field in measure.xml
   def name
-    return "ThermostatBias"
+    return "Thermostat Bias"
   end
   
   # human readable description
