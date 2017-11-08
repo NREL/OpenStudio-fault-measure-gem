072addf626509d862c9fe430f803fdc04a037bd0
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2017-11-08 15:44:30 -0500

    Update measure.rb

--- fault_measures_2017/HVACSetbackErrorDelayedOnset/measure.rb
+++ fault_measures_2017/HVACSetbackErrorDelayedOnset/measure.rb
@@ -21,7 +21,7 @@ class ExtendEveningThermostatSetpointWeek < OpenStudio::Ruleset::ModelUserScript
   # define the name that a user will see, this method may be deprecated as
   # the display name in PAT comes from the name field in measure.xml
   def name
-    return 'ExtendEveningThermostatSetpointWeek'
+    return 'HVAC Setback Error: Delayed Onset'
   end
 
   # simple human readable description
