7c41d0e31dbf1a40fd18c7ce845728eebd35e830
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2017-11-13 15:59:58 -0500

    Update measure.rb

--- fault_measures_2017/ExcessiveInfiltration/measure.rb
+++ fault_measures_2017/ExcessiveInfiltration/measure.rb
@@ -13,7 +13,7 @@ class ExcessiveInfiltration < OpenStudio::Ruleset::ModelUserScript
   #define the name that a user will see, this method may be deprecated as
   #the display name in PAT comes from the name field in measure.xml
   def name
-    return "ExcessiveInfiltration"
+    return "Excessive Infiltration"
   end
   
   # human readable description
