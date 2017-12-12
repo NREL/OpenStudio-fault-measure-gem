f3117dad5873dafd6fe8a8331b5e281c2fd0a26f
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2017-12-12 11:29:04 -0500

    Update measure.rb

--- fault_measures_2017/CondenserFouling/measure.rb
+++ fault_measures_2017/CondenserFouling/measure.rb
@@ -302,7 +302,7 @@ class CondenserFouling < OpenStudio::Ruleset::WorkspaceUserScript
   end
 
   def _return_err_message_for_not_unit(runner, existing_coils, coil_choice)
-    runner.registerError("Measure RTULLWithBfOffset cannot find #{coil_choice}. Exiting......")
+    runner.registerError("Measure CondenserFouling cannot find #{coil_choice}. Exiting......")
     coils_msg = 'Only coils '
     existing_coils.each do |existing_coil|
       coils_msg += (existing_coil + ', ')
