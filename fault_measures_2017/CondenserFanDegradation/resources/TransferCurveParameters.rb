3a3d54ff0d2f07e80d6a71c53cf36f2549523b69
David Goldwasser <david.f.goldwasser@nrel.gov>
2018-03-20 11:17:50 -0600

    unexpected local chagnes
    what is proposed matches what is currently on GitHub.com

--- fault_measures_2017/CondenserFanDegradation/resources/TransferCurveParameters.rb
+++ fault_measures_2017/CondenserFanDegradation/resources/TransferCurveParameters.rb
@@ -1,19 +1,16 @@
-#This ruby script contains functions that transfers parameters
-#from the curves defined in EnergyPlus to EnergyPlus Measure script
-#for the creation of EnergyManagementSystem script
+# This ruby script contains functions that transfers parameters
+# from the curves defined in EnergyPlus to EnergyPlus Measure script
+# for the creation of EnergyManagementSystem script
 
-#define function to get parameters from biquadratic curves
+# define function to get parameters from biquadratic curves
 def para_biquadratic(curvebiquadratics, curve_name)
   para = []
   no_curve = true
   curvebiquadratics.each do |curvebiquadratic|
     if curvebiquadratic.getString(0).to_s.eql?(curve_name)
-      para << curvebiquadratic.getString(1).to_s
-      para << curvebiquadratic.getString(2).to_s
-      para << curvebiquadratic.getString(3).to_s
-      para << curvebiquadratic.getString(4).to_s
-      para << curvebiquadratic.getString(5).to_s
-      para << curvebiquadratic.getString(6).to_s
+      (1..6).each do |ii|
+        para << curvebiquadratic.getString(ii).to_s
+      end
       no_curve = false
       break
     end
@@ -21,13 +18,13 @@ def para_biquadratic(curvebiquadratics, curve_name)
   return curve_name, para, no_curve
 end
 
-#define function to get parameters from biquadratic curves, including limits to x and y
+# define function to get parameters from biquadratic curves, including limits to x and y
 def para_biquadratic_limit(curvebiquadratics, curve_name)
   para = []
   no_curve = true
   curvebiquadratics.each do |curvebiquadratic|
     if curvebiquadratic.getString(0).to_s.eql?(curve_name)
-      for i in 1..10
+      (1..10).each do |i|
         para << curvebiquadratic.getString(i).to_s
       end
       no_curve = false
