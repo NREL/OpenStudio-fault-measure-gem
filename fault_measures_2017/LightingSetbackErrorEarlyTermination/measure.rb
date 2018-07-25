4e87d12762d75164f02e33a4bb346e9f3b55d1da
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2018-07-25 10:47:24 -0400

    Update measure.rb
    Fault description updated.

--- fault_measures_2017/LightingSetbackErrorEarlyTermination/measure.rb
+++ fault_measures_2017/LightingSetbackErrorEarlyTermination/measure.rb
@@ -29,7 +29,7 @@ class LightingSetbackErrorEarlyTermination < OpenStudio::Ruleset::ModelUserScrip
 
   # simple human readable description
   def description
-    return "Lighting should be turned off or at least reduced during unoccupied hours. However, some commissioning studies have found noticeable lighting energy use at night either because lighting schedules are improperly configured or occupants forget to turn off lights when vacating a building (Haasl, Stum, and Arney 1996; Kahn, Potter, and Haasl 2002). This measure simulates the effect of the lighting setback being terminated earlier during unoccupied hours by modifying the Schedule:Compact object in EnergyPlus assigned to lighting schedules. The fault intensity (F) for this fault is defined as the early termination of overnight lighting setback (in hours), which is specified as one of the inputs."
+    return "Lighting should be turned off or at least reduced during unoccupied hours. However, some commissioning studies have found noticeable lighting energy use at night either because lighting schedules are improperly configured or occupants forget to turn off lights when vacating a building. This fault is categorized as a fault that occur in the lighting system (controller) during the operation stage. This fault measure is based on a physical model where certain parameter(s) is changed in EnergyPlus to mimic the faulted operation; thus simulates the effect of the lighting setback being terminated earlier during unoccupied hours by modifying the Schedule:Compact object in EnergyPlus assigned to lighting schedules. The fault intensity (F) is defined as the early termination of overnight lighting setback (in hours)."
   end
 
   # detailed human readable description about how to use the measure
